f3a2845f01ddd8387479fb2e27a0962ea827cf5c
taisto <taisto.qvist@gmail.com>
2020-01-24 15:29:11 +0100

    fri-main-filter-working

--- filter-yaml.py
+++ filter-yaml.py
@@ -3,47 +3,18 @@
 import json
 import os, sys, re
 import argparse
+from time import sleep
 
-_scriptVersion=0.01
+from samba.dcerpc.smb_acl import entry
+
+_scriptVersion=0.1
 
 
 _FILE_DEV_REGISTRY="/var/lib/ha/.storage/core.device_registry"
 _FILE_ENTITY_REGISTRY="/var/lib/ha/.storage/core.entity_registry"
-
-def _createArgParser():
-    cli = argparse.ArgumentParser(
-            prog="PythonProg",
-            description = "Default Description",
-            epilog="This is a python script",
-            )
-
-    cli.add_argument('--version', "-V", action='version', version=F'%(prog)s {_scriptVersion}')
-    #cli.add_argument('--out', "-o", nargs='?', default=sys.stdout)  #type=argparse.FileType('w')
-
-    cli.add_argument('--nullcount', "-nc", default=3)
-    cli.add_argument('--list-pattern', "-lp")  # List fields from registry files.
-    cli.add_argument('--unify', "-U", help="match default unifi entries")  # List fields from registry files.
-
-    fromWhere = cli.add_mutually_exclusive_group()
-    fromWhere.add_argument('--device', "-d", action="store_true", default=False)
-    fromWhere.add_argument('--stdin', "-i", action="store_true", default=False)
-    fromWhere.add_argument('--file', "-f", dest="infile")
-    fromWhere.add_argument('--entity', "-e", action="store_true", default=False) # This should be default...?
-
-    howFilter = cli.add_mutually_exclusive_group()
-    # USAGE: filter.py -p uuid:XX, plattform, dev (or -p X:uuid:".*aa"
-    howFilter.add_argument('--pattern', "-p", nargs="*", metavar="key:filter")
-
-    whatDoFilter = cli.add_mutually_exclusive_group()
-    whatDoFilter.add_argument("--exclude", "-x", action="store_true", default=True)
-    whatDoFilter.add_argument("--include", "-I", action="store_true", default=False)
-    #whatDoFilter.add_argument('--list', "-l", help="list only", action="store_true")
-
-    loggingDebug = cli.add_mutually_exclusive_group() # required=true if either cli-param is required
-    loggingDebug.add_argument("--verbose", "-v", help="Increase verbosity", action='count')
-    loggingDebug.add_argument("--quiet", "-q", help="Reduce verbosity", action='count')
-    return cli
-
+_FILE_OUT_STRIPPED="./stripped.json"
+_FILE_OUT_CLEANED="./cleaned.json"
+LogLevel = 0
 
 
 def printEntity(e, e_id, all_meta=False):
@@ -54,124 +25,142 @@ def printEntity(e, e_id, all_meta=False):
     print("")
 
 
-def processEntities(args, jsonData):
+def matchEntry(entry, entry_filters : dict) -> bool:
+    """ TODO """
+    #print("CCC", entry["connections"], type(entry["connections"]))
+    assert isinstance(entry_filters, dict) and len(entry_filters) > 0
+    # TODO: Print warning if a filterKey matches multiple entryKeys? ==> WILL OFTEN AUTOFAIL!
+    for filterKey in entry_filters.keys():
+        filterKeyFound = False
+        for entryKey in entry.keys():
+            if re.search(filterKey, entryKey, re.IGNORECASE):
+                if LogLevel > 3:
+                    print(f"KeyCheck [{entryKey}] against [{filterKey}] - MATCH")
+                filterKeyFound = True
+                # Here, we've found user-filter-key that matches a key for a specific entry.
+                # If the values for those keys dont match, this entry will never match.
+                # (But the entry may be empty)
+                if isinstance(entry[entryKey], dict):
+                    # Recursively search datastructure. TODO:
+                    pass
+                elif isinstance(entry[entryKey], list) or isinstance(entry[entryKey], tuple) :
+                    # Recursively search datastructure. TODO:
+                    pass
+                elif isinstance(entry[entryKey], int):
+                    if entry[entryKey] != int(entry_filters[filterKey]):
+                        print("ValueFail:", entry[entryKey], entry_filters[filterKey])
+                    pass
+                else: # String Datatype (TODO:Float...)
+                    if re.search("null", entry_filters[filterKey], re.IGNORECASE):
+                        if entry[entryKey] is None or len(entry[entryKey]) == 0:
+                            continue
+                        else:
+                            return False
+                    else:
+                        entryValue = entry[entryKey] if entry[entryKey] is not None else ""
+                        if not re.search(entry_filters[filterKey], entryValue):
+                            if LogLevel > 3:
+                                print(f"ValueCheck [{entryValue}] against [{entry_filters[filterKey]}] - MIS-matches", )
+                            return False
+                        else:
+                            if LogLevel > 2: print(f"ValueCheck [{entryValue}] against [{entry_filters[filterKey]}] - MATCHES", )
+            else:
+                if LogLevel > 2: print(f"KeyCheck [{entryKey}] against [{filterKey}] == MISmatch")
+
+        if not filterKeyFound:
+            if LogLevel > 0: print(f"Filterkey Did NOT match any entry-key: [{filterKey}]")
+            # The user specified at least one key:value, but it wasn't found, so the entry cant match!
+            return False
+        else:
+            # So filter key WAS found, but there are more filterKeys
+            pass
+
+    # If no filters for stripping matches...
+    return True
+
+
+def copyAllButEntries(orgDict: dict(), entry_key: str) -> dict:
     '''
-    Process /var/lib/home-assistant/.storage/core.entity_registry
+    Dict structure is:
     '''
+    copyOfDict = dict()
+    # Normally "key", "data", "version"
+    for key in orgDict.keys():
+        assert isinstance(key, str)
+        if key == "data" :
+            copyOfDict[key] = {}
+            assert isinstance(orgDict[key], dict)
+            dataKeys = list(orgDict[key].keys())
+            assert len(dataKeys) == 1, "There should only be a single key here"
+            assert dataKeys[0] == entry_key
+            assert isinstance(orgDict[key][entry_key], list)
+            copyOfDict[key][entry_key] = []
+        else:
+            copyOfDict[key] = orgDict[key]
+    return copyOfDict
+
+
+def processEntities(jsonData: dict(), entry_key: str, entryFilters: dict, **kwargs):
+    '''
+    Process /var/lib/home-assistant/.storage/core.{jsonKey}_registry
+    '''
+
     fullCount = 0
-    unifiCount = 0
-    deleteCount = 0
-    filteredData = dict()
-    filteredData["data"] = dict()
-    filteredData["data"]["entities"] = []
-    deleteData = dict()
-    deleteData["data"] = dict()
-    deleteData["data"]["entities"] = []
+    stripCount = 0
+    whatsLeft = copyAllButEntries(jsonData, entry_key)
+    filteredData = copyAllButEntries(jsonData, entry_key)
+    entriesToKeep = []
+    entiresToStrip= []
+    origCount = len(jsonData["data"][entry_key])
+    print("Will filter with filters", entryFilters)
+    #sleep(2)
+    for entry in jsonData["data"][entry_key] :
+        if matchEntry(entry, entryFilters):
+            if LogLevel > 1: printEntity(entry, "Stripping\n", json.dumps(entry, indent=2, sort_keys=True))
+            entiresToStrip.append(entry)
+        else:
+            #out = json.dumps(entry, indent=2, sort_keys=True)
+            if LogLevel > 1: printEntity(entry, "Appending\n", json.dumps(entry, indent=2, sort_keys=True))
+            entriesToKeep.append(entry)
+        fullCount +=1
+    whatsLeft["data"][entry_key] = entriesToKeep
+    filteredData["data"][entry_key] = entiresToStrip
+    print(f"\nOrig entry Count: {origCount}, Current (filtered) count: {len(entriesToKeep)}, StripCount: {len(entiresToStrip)}\n")
 
-    origCount = len(jsonData["data"]["entities"])
-
-    if args.list_node :
-        for entity in jsonData["data"]["entities"] :
-            filterString1 = str(args.list_node) + "-"
-            filterString2 = "node-" + str(args.list_node)
-            if re.match(filterString1, entity["unique_id"]) or re.match(filterString2, entity["unique_id"]) :
-                #printEntity(entity, fullCount, all_meta=True)
-                print(json.dumps(entity, indent=1))
-                print("")
-                fullCount += 1
-
-    if args.del_node :
-        for entity in jsonData["data"]["entities"] :
-            filterString1 = str(args.del_node) + "-"
-            filterString2 = "node-" + str(args.del_node)
-            if re.match(filterString1, entity["unique_id"]) or re.match(filterString2, entity["unique_id"]) :
-                deleteData["data"]["entities"].append(entity)
-                print(json.dumps(entity, indent=1))
-                print("")
-                deleteCount += 1
-            else:
-                filteredData["data"]["entities"].append(entity)
-                fullCount +=1
-        afterFile = open("/tmp/after.yaml", "w")
-        delFile = open("/tmp/deleted.yaml", "w")
-        print("-------------------")
-        print("Filterd Count: {}, Deleted Count: {}, Original Count: {}".
-                format(fullCount, deleteCount, origCount))
-        json.dump(filteredData, afterFile, indent=2, sort_keys=True)
-        afterFile.close()
-        json.dump(deleteData, delFile, indent=2, sort_keys=True)
-        delFile.close()
-
-    if args.list_disabled : # Disabled_by is not empty 
-        for entity in jsonData["data"]["entities"] :
-            if entity["disabled_by"] :
-                printEntity(entity, fullCount)
-                print("")
-                fullCount += 1
-    
-    if args.list_unifi != "dont" :
-        for entity in jsonData["data"]["entities"] :
-            if entity["platform"] == "unifi":
-                # Should we only display the known2empty and is it one of those?
-                if re.match(r"only", args.list_unifi) :
-                    if re.match(r"device_tracker.unifi.*", entity["entity_id"]):
-                        printEntity(entity, fullCount)
-                        unifiCount += 1
-                else :
-                    printEntity(entity, fullCount)                                            
-                fullCount += 1
-        print("All Unifi Count: {}, Filtered Unifi Count: {}".format(fullCount, unifiCount))
+    cleanedFile = open(_FILE_OUT_CLEANED, "w")
+    strippedFile = open(_FILE_OUT_STRIPPED, "w")
+    #print("-------------------")
+    json.dump(whatsLeft, cleanedFile, indent=2, sort_keys=True)
+    json.dump(filteredData, strippedFile, indent=2, sort_keys=True)
+    cleanedFile.close()
+    strippedFile.close()
+    os.system("ls -lF *.json")
+    #print("-------------------")
 
 
-    if args.strip_unifi != "dont" :
-        '''
-        here we've got entities[] with keys:
-        - "config_entry_id": "7bfa7234010b419f956f6dbb9d72e966",
-        - "device_id": "9f718999a08345db9123fd20c929344c",
-        - "disabled_by": "user",
-        - "entity_id": "sensor.fibaro_system_fgms001_zw5_motion_sensor_seismic_intensity",
-        - "entity_id": "device_tracker.unifi_58_ef_39_a8_3a_9e_default",        
-        - "name": "MS - Mancave - Seismic",
-        - "platform": "zwave",
-        - "unique_id": "3-72057594093257106"
-        '''
-        print("Will strip {} Unifi entries".format(args.strip_unifi))
-        for entity in jsonData["data"]["entities"] :
-            if entity["platform"] == "unifi" :
-                # Should we only strip the empty, know-2-bad entries?
-                if re.match(r"only", args.strip_unifi) and not re.match(r"device_tracker.unifi.*", entity["entity_id"]):
-                    print("Adding: {} with name: {}".format(entity["entity_id"], entity["name"]))
-                    filteredData["data"]["entities"].append(entity)
-                    unifiCount += 1
-            else:
-                filteredData["data"]["entities"].append(entity)
-            fullCount +=1
-    
-        print("Filtered list count: {}, Orig List Count: {}, Unifi Count: {}".
-                format(len(filteredData["data"]["entities"]), origCount, unifiCount))
-        newFile = open("/tmp/newfile.yaml", "w")
-        print("-------------------")
-        json.dump(filteredData, newFile, indent=2, sort_keys=True)
-        newFile.close()
-        print("-------------------")
+def listFilterKeywords(dataKey: str, jsonData : dict):
+    """
+    This function will parse the Json-File and print out the keys for each entitity[dict], so make it easier
+    to create match-patterns with --pattern <key:value>
 
-def listFilterKeywords(which : str, jsonData : dict):
-    searchList = []
-    if "devices" in jsonData["data"]:
-        searchList = jsonData["data"]["devices"]
-    elif "entities" in jsonData["data"] :
-        searchList = jsonData["data"]["entities"]
-    else:
-        print("Did not find either DEVICES or ENTITIES in", jsonData["data"].keys())
+    :param dataKey:
+    :param jsonData:
+    :return:
+    """
 
-    keysFoundInList = [ key for key in searchList[0].keys()]
+    assert isinstance(jsonData["data"], dict) and dataKey in jsonData["data"].keys(), "Invalid DICT for parsing"
+    searchList = jsonData["data"][dataKey]
+
+    keysFoundInList = [key for key in searchList[0].keys()]
     #print("Keys", keysFoundInList)
-    # This loop is probably unnecessary since all entries SHOULD have the SAME keys.
+
+    # This loop is probably unnecessary since all entries SHOULD have the SAME keys, but, better safe than sorry.
     foundAdditional = False
     for entry in searchList:
         for entryKey in entry.keys():
             if entryKey not in keysFoundInList:
-                print("Adding new key", entry)
+                print("SOME ENTRIES HAVE DIFFERENT AMOUNT OF KEYS", entry)
+                sleep(2.0)
                 foundAdditional = True
                 keysFoundInList.append(entryKey)
 
@@ -179,16 +168,55 @@ def listFilterKeywords(which : str, jsonData : dict):
         print(f"Key: {k}")
     # TODO: Add printout of common values (use values["key"] = set())
 
+def _createArgParser():
+    cli = argparse.ArgumentParser(
+            #prog="Ha-Storage-Filter",
+            description = "This program will filter Home-assistants .storage/-files",
+            epilog="""
+            Use this program at your OWN RISK. For safety sake, it will never overwrite the original files.\
+            
+            Additionally, it is currently only capable of filtering string-data in the json files\
+            """)
+
+    cli.add_argument('--version', "-V", action='version', version=F'%(prog)s {_scriptVersion}')
+    #cli.add_argument('--out', "-o", nargs='?', default=sys.stdout)  #type=argparse.FileType('w')
+
+    cli.add_argument('--nullcount', "-nc", default=3)
+    cli.add_argument('--list-pattern', "-l", "-lp", nargs="?", default=None, const=".*")
+    cli.add_argument('--unify', "-U", help="match default unifi entries")  # List fields from registry files.
+
+    fromWhere = cli.add_mutually_exclusive_group(required=True)
+    fromWhere.add_argument('--device', "-d", action="store_true", default=False)
+    fromWhere.add_argument('--stdin', "-i", action="store_true", default=False)
+    fromWhere.add_argument('--file', "-f", dest="infile")
+    fromWhere.add_argument('--entity', "-e", action="store_true", default=False) # This should be default...?
+
+    #howFilter = cli.add_mutually_exclusive_group()
+    # USAGE: filter.py -p uuid:XX, plattform, dev (or -p X:uuid:".*aa"
+    cli.add_argument('--pattern', "-p", nargs="*", metavar="key:filter", required=True)
+
+    whatDoFilter = cli.add_mutually_exclusive_group()
+    whatDoFilter.add_argument("--exclude", "-x", action="store_true", default=True)
+    whatDoFilter.add_argument("--include", "-I", action="store_true", default=False)
+    #whatDoFilter.add_argument('--list', "-l", help="list only", action="store_true")
+
+    loggingDebug = cli.add_mutually_exclusive_group() # required=true if either cli-param is required
+    loggingDebug.add_argument("--verbose", "-v", help="Increase verbosity", action='count', default=0)
+    loggingDebug.add_argument("--quiet", "-q", help="Reduce verbosity", action='count', default=0)
+    return cli
+
+
 def main(argv = None):
     _scriptName = sys.argv[0]
     cliArgs = _createArgParser()
-
-    print(f"Arguments Will BE parsed by {sys.argv[0]}, and version:{_scriptVersion}")
     args = cliArgs.parse_args()
-    print(args)
+    #print(args)
 
+    global LogLevel
+    LogLevel += int(args.verbose) - args.quiet
+    #print(f"Arguments Will BE parsed by {sys.argv[0]}, and version:{_scriptVersion}, LogLevel {LogLevel}")
     input_source = ""
-    jsonDataKey = ""
+    dataKey = ""
     if args.entity :
         input_source = _FILE_ENTITY_REGISTRY
     elif args.device:
@@ -209,59 +237,28 @@ def main(argv = None):
         except:
             print("Failed to open file")
 
-    jsonDataKey = list(jsonObj["data"].keys())[0]
-    count = len(jsonObj["data"][jsonDataKey])
-    print(f"\nReading data for [{jsonDataKey}] Count: {count}\n")
-    #sys.exit()
-    if "entities" in jsonObj["data"].keys():
-        listFilterKeywords("entities", jsonObj)
-    elif "devices" in jsonObj["data"].keys():
-        listFilterKeywords("devices", jsonObj )
-    else :
-        print("Unknown contents in file")
+    topKeys = list(jsonObj.keys())
+    dataKeys = list(jsonObj["data"].keys())
+    dataKey = dataKeys[0]
+    count = len(jsonObj["data"][dataKey])
 
-        
+    #print(f"\nReading data for [{dataKey}] Count: {count} With Data Keys: {dataKeys}, and TopKeys {topKeys}")
+    # for w in topKeys:
+    #     if isinstance(jsonObj[w], dict):
+    #         print(f"Key [{w}]: Type [{type(jsonObj[w])}] Id: {jsonObj[w].keys()}\n")
+    #     else:
+    #         print(f"Key [{w}], Id: {jsonObj[w]}\n")
+    if args.list_pattern :
+        listFilterKeywords(dataKey, jsonObj)
+    else:
+        patternHash = dict()
+        for p in args.pattern:
+            key, value = p.split(":")
+            patternHash[key] = value
+        processEntities(jsonObj, dataKey, patternHash ) # {"plat" : "zwave", "_id": "default"}
+    print(f"\nCompleted processing. You should now REALLY compare the files with the original. Like: \n"
+          f"kdiff3 {input_source} {_FILE_OUT_CLEANED}")
+
 if __name__ == "__main__":
     sys.exit(main())
 
-
-
-
-"""
-
-Entity Registry Entry: /var/lib/ha/.storage/core.entity_registry     
-    {
-                "capabilities": null,
-                "config_entry_id": "693fca7e7aa2446e9665416ff0f62de4",
-                "device_class": null,
-                "device_id": "1c52eda6d9654218bb48f9e5fcebb94f",
-                "disabled_by": null,
-                "entity_id": "device_tracker.unifi_cc_5e_03_4b_d3_09_default",
-                "name": null,
-                "platform": "unifi",
-                "supported_features": 0,
-                "unique_id": "cc:5e:03:4b:d3:09-default",
-                "unit_of_measurement": null
-    }
-    Device registry entry: / var / lib / ha /.storage / core.device_registry
-    {
-        "area_id": null,
-        "config_entries": [
-            "693fca7e7aa2446e9665416ff0f62de4"
-        ],
-        "connections": [
-            [
-                "mac",
-                "cc:5e:03:4b:d3:09"
-            ]
-        ],
-        "id": "1c52eda6d9654218bb48f9e5fcebb94f",
-        "identifiers": [],
-        "manufacturer": null,
-        "model": null,
-        "name": null,
-        "name_by_user": null,
-        "sw_version": null,
-        "via_device_id": null
-    }
-"""
---
+++ m
@@ -0,0 +1,21 @@
+{
+    "data": {
+        "entities": [
+            {
+                "capabilities": {},
+                "config_entry_id": "693fca7e7aa2446e9665416ff0f62de4",
+                "device_class": null,
+                "device_id": "3ad61e840633455f8315b5388806f35b",
+                "disabled_by": "integration",
+                "entity_id": "sensor.none_tx_287",
+                "name": null,
+                "platform": "unifi",
+                "supported_features": 0,
+                "unique_id": "tx-7e:12:4c:f5:6c:60",
+                "unit_of_measurement": null
+            }
+        ]
+    },
+    "key": "core.entity_registry",
+    "version": 1
+}
